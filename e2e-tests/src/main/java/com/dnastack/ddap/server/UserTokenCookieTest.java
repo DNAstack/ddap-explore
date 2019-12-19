8337f09923588018fde4711b11f20bcadfc86e24
Milan Panik <milan.panik1@gmail.com>
2019-12-19 12:41:26 +0100

    DISCO-2590 Update e2e tests for stale cookies

--- e2e-tests/src/main/java/com/dnastack/ddap/server/UserTokenCookieTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/server/UserTokenCookieTest.java
@@ -32,26 +32,6 @@ public class UserTokenCookieTest extends AbstractBaseE2eTest {
         setupRealmConfig(TestingPersona.ADMINISTRATOR, damConfig, "1", REALM);
     }
 
-    @Test
-    public void damResponsesShouldClearExpiredUserTokenCookies() throws Exception {
-        String expiredUserTokenCookie = fakeUserToken(Instant.now().minusSeconds(10));
-
-        // @formatter:off
-        getRequestSpecification()
-            .log().method()
-            .log().cookies()
-            .log().uri()
-            .cookie("dam_token", expiredUserTokenCookie)
-        .when()
-            .get(damViaDdap("/resources/resource-name/views/view-name"))
-        .then()
-            .log().body()
-            .log().ifValidationFails()
-            .statusCode(isOneOf(401, 404))
-            .cookie("dam_token", "expired");
-        // @formatter:on
-    }
-
     private String damViaDdap(String path) {
         return format("/dam/v1alpha/%s%s", REALM, path);
     }
@@ -129,25 +109,109 @@ public class UserTokenCookieTest extends AbstractBaseE2eTest {
         // @formatter:on
     }
 
+    @Test
+    public void expiredDamTokenShouldExpireUserTokenCookies() throws Exception {
+        String expiredUserTokenCookie = fakeUserToken(Instant.now().minusSeconds(10));
+
+        // @formatter:off
+        getRequestSpecification()
+            .log().method()
+            .log().cookies()
+            .log().uri()
+            .cookie("dam_token", expiredUserTokenCookie)
+            .when()
+            .get(damViaDdap("/resources/resource-name/views/view-name"))
+            .then()
+            .log().body()
+            .log().ifValidationFails()
+            .statusCode(isOneOf(401, 404))
+            .cookie("dam_token", "expired");
+        // @formatter:on
+    }
+
+    @Test
+    public void expiredIcTokenShouldExpireUserTokenCookies() throws Exception {
+        String expiredUserTokenCookie = fakeUserToken(Instant.now().minusSeconds(10));
+
+        // @formatter:off
+        getRequestSpecification()
+            .log().method()
+            .log().cookies()
+            .log().uri()
+            .cookie("ic_token", expiredUserTokenCookie)
+            .when()
+            .get(damViaDdap("/accounts/-"))
+            .then()
+            .log().body()
+            .log().ifValidationFails()
+            .statusCode(isOneOf(401, 404))
+            .cookie("ic_token", "expired");
+        // @formatter:on
+    }
+
+    @Test
+    public void staleDamTokenShouldExpireUserTokenCookies() throws Exception {
+        String expiredUserTokenCookie = fakeClearTextUserToken(Instant.now().minusSeconds(10));
+
+        // @formatter:off
+        getRequestSpecification()
+            .log().method()
+            .log().cookies()
+            .log().uri()
+            .cookie("dam_token", expiredUserTokenCookie)
+            .when()
+            .get(damViaDdap("/resources/resource-name/views/view-name"))
+            .then()
+            .log().body()
+            .log().ifValidationFails()
+            .statusCode(isOneOf(401, 404))
+            .cookie("dam_token", "expired");
+        // @formatter:on
+    }
+
+    @Test
+    public void staleIcTokenShouldExpireUserTokenCookies() throws Exception {
+        String expiredUserTokenCookie = fakeClearTextUserToken(Instant.now().minusSeconds(10));
+
+        // @formatter:off
+        getRequestSpecification()
+            .log().method()
+            .log().cookies()
+            .log().uri()
+            .cookie("ic_token", expiredUserTokenCookie)
+            .when()
+            .get(icViaDdap("/accounts/-"))
+            .then()
+            .log().body()
+            .log().ifValidationFails()
+            .statusCode(isOneOf(401, 404))
+            .cookie("ic_token", "expired");
+        // @formatter:on
+    }
+
     private String icViaDdap(String path) {
         return format("/identity/v1alpha/%s%s", REALM, path);
     }
 
     private String fakeUserToken(Instant exp) throws JsonProcessingException {
         TextEncryptor encryptor = Encryptors.text(DDAP_COOKIES_ENCRYPTOR_PASSWORD, DDAP_COOKIES_ENCRYPTOR_SALT);
+        return encryptor.encrypt(fakeClearTextUserToken(exp));
+    }
+
+    private String fakeClearTextUserToken(Instant exp) throws JsonProcessingException {
         // Note this will only work so long as DDAP frontend uses unencrypted DAM access tokens as cookie value
         ObjectMapper jsonMapper = new ObjectMapper();
         Base64.Encoder b64Encoder = Base64.getUrlEncoder().withoutPadding();
 
         Map<String, Object> header = ImmutableMap.of(
-                "typ", "JWT",
-                "alg", "none");
+            "typ", "JWT",
+            "alg", "none");
         Map<String, Object> body = ImmutableMap.of(
-                "exp", exp.getEpochSecond());
+            "exp", exp.getEpochSecond());
 
-        return encryptor.encrypt(b64Encoder.encodeToString(jsonMapper.writeValueAsBytes(header)) +
+        return b64Encoder.encodeToString(jsonMapper.writeValueAsBytes(header)) +
             "." +
             b64Encoder.encodeToString(jsonMapper.writeValueAsBytes(body)) +
-            ".");
+            ".";
     }
 }

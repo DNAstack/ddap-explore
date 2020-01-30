617258597bfab82e2d633e2e50ec71d174ad6122
Milan Panik <milan.panik1@gmail.com>
2020-01-30 10:57:56 +0100

    Update protos

--- angular/scripts/generate_proto.sh
+++ angular/scripts/generate_proto.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 COMMON_PROTO_FILE=../shared/protos/proto/common/v1/common.proto
+COMMON_OAUTHCLIENT_PROTO_FILE=../shared/protos/proto/common/v1/oauthclient.proto
 SRC_DAM_PROTO_FILE=../shared/protos/dam_service.proto
 SRC_IC_PROTO_FILE=../shared/protos/ic_service.proto
 OUT_DAM_FILE=dam-service
@@ -23,11 +24,11 @@ mkdir -p ${OUT_DIR}
 # DAM
 cp ${SRC_DAM_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_DAM_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_DAM_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE} ${COMMON_OAUTHCLIENT_PROTO_FILE}
 ${PBTS_BIN} -o ${OUT_DIR}/${OUT_DAM_FILE}.d.ts ${OUT_DIR}/${OUT_DAM_FILE}.js
 
 # IC
 cp ${SRC_IC_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_IC_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_IC_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE} ${COMMON_OAUTHCLIENT_PROTO_FILE}
 ${PBTS_BIN} -o ${OUT_DIR}/${OUT_IC_FILE}.d.ts ${OUT_DIR}/${OUT_IC_FILE}.js

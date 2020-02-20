41491fb6fbdef98a17f5f05e6cef43f2f6ab6781
Juti Noppornpitak <jnopporn@shiroyuki.com>
2020-02-20 14:14:25 -0500

    Reordered and fixed routes in analytics (+ fixed regression bugs)

--- angular/src/app/workflows/workflows-routing.module.ts
+++ angular/src/app/workflows/workflows-routing.module.ts
@@ -8,13 +8,14 @@ import { WorkflowListSingleComponent } from './workflow-list-single/workflow-lis
 import { WorkflowManageComponent } from './workflow-manage/workflow-manage.component';
 
 export const routes: Routes = [
-  { path: ':damId/views/:viewId/runs', component: WorkflowListSingleComponent},
-  { path: ':damId/views/:viewId/runs/:runId', component: WorkflowDetailComponent},
-  { path: ':damId/views/:viewId/runs/manage/add', component: WorkflowManageComponent},
+  { path: '', redirectTo: 'operations' },
+  { path: 'operations/:damId/views/:viewId/runs', component: WorkflowListSingleComponent},
+  { path: 'operations/:damId/views/:viewId/runs/:runId', component: WorkflowDetailComponent},
+  { path: 'operations/:damId/views/:viewId/runs/manage/add', component: WorkflowManageComponent},
   { path: 'run', pathMatch: 'full', component: WorkflowManageComponent },
   { path: 'run/with', component: WorkflowManageComponent },
   { path: 'workflows', component: RegisteredWorkflowListComponent },
-  { path: '', component: WorkflowListMultiComponent },
+  { path: 'operations', component: WorkflowListMultiComponent },
 ];
 
 @NgModule({

d3f3f902f3ba464b97081088c58d9c13d49244c5
Monica <monica@dnastack.com>
2020-03-16 19:41:08 -0400

    search query

--- angular/src/app/search/search.module.ts
+++ angular/src/app/search/search.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
+import { _MatMenuDirectivesModule, MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -10,10 +13,16 @@ import { DdapLayoutModule } from 'ddap-common-lib';
 import { AceEditorModule } from 'ng2-ace-editor';
 
 import { SearchRoutingModule } from './search-routing.module';
+import { LinkifyPipe } from './search-tables/linkify.pipe';
+import { ObjectToArrayPipe } from './search-tables/objecttoarray.pipe';
 import { SearchTablesComponent } from './search-tables/search-tables.component';
 
 @NgModule({
-  declarations: [SearchTablesComponent],
+  declarations: [
+    SearchTablesComponent,
+    ObjectToArrayPipe,
+    LinkifyPipe,
+  ],
   imports: [
     SearchRoutingModule,
     DdapLayoutModule,
@@ -25,6 +34,10 @@ import { SearchTablesComponent } from './search-tables/search-tables.component';
     AceEditorModule,
     MatTooltipModule,
     MatButtonModule,
+    MatChipsModule,
+    _MatMenuDirectivesModule,
+    MatMenuModule,
+    MatProgressBarModule,
   ],
 })
 export class SearchModule {}
--- src/main/java/com/dnastack/ddap/explore/search/client/SearchClient.java
+++ src/main/java/com/dnastack/ddap/explore/search/client/SearchClient.java
@@ -3,21 +3,37 @@ package com.dnastack.ddap.explore.search.client;
 import com.dnastack.ddap.common.client.WebClientFactory;
 import com.dnastack.ddap.explore.search.model.SearchTablesResponseModel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 
 @Slf4j
 @Component
 public class SearchClient {
 
     public Mono<SearchTablesResponseModel> getTables(URI resourceUri) {
-        log.info("Search client get tables is called");
         return WebClientFactory.getWebClient()
                 .get()
                 .uri(resourceUri)
                 .retrieve()
                 .bodyToMono(SearchTablesResponseModel.class);
+    }
+
+    public Mono<Object> query(URI resourceUri, Map<String, String> queryData) {
+        log.info("Search client make query request {}", queryData.get("query"));
+        return WebClientFactory.getWebClient()
+                .post()
+                .uri(resourceUri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(queryData))
+                .retrieve()
+                .bodyToMono(Object.class);
     }
 }

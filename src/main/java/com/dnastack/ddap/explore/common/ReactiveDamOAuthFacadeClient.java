7e8244446bb683a1730a9b3365d6c1f5e20544ed
patrick <patrick@dnastack.com>
2020-03-18 14:28:48 -0400

    Got DDAP to work with a generic IDP, specifically wallet

--- src/main/java/com/dnastack/ddap/explore/common/ReactiveDamOAuthFacadeClient.java
+++ src/main/java/com/dnastack/ddap/explore/common/ReactiveDamOAuthFacadeClient.java
@@ -1,11 +1,18 @@
 package com.dnastack.ddap.explore.common;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import com.dnastack.ddap.common.client.WebClientFactory;
 import com.dnastack.ddap.common.security.InvalidTokenException;
 import com.dnastack.ddap.explore.common.config.DamFacadeConfig;
+import com.dnastack.ddap.explore.common.config.DamFacadeConfig.Oauth2;
 import com.dnastack.ddap.explore.dam.client.ReactiveDamOAuthClient;
 import com.dnastack.ddap.ic.oauth.client.TokenExchangeException;
 import com.dnastack.ddap.ic.oauth.model.TokenResponse;
+import java.net.URI;
+import java.util.Base64;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -13,35 +20,32 @@ import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.Base64;
-import java.util.List;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 @Slf4j
 public class ReactiveDamOAuthFacadeClient implements ReactiveDamOAuthClient {
+
     private final DamFacadeConfig damFacadeConfig;
+    private final Oauth2 oauth2Config;
 
     public ReactiveDamOAuthFacadeClient(DamFacadeConfig damFacadeConfig) {
         this.damFacadeConfig = damFacadeConfig;
+        this.oauth2Config = damFacadeConfig.getOauth2();
     }
 
     @Override
-    public URI getAuthorizeUrl(String realm, String state, String scopes, URI redirectUri, List<URI> resources, String loginHint, String ttl) {
+    public URI getAuthorizeUrl(String realm, String state, String scopes, URI redirectUri, List<URI> resources, String loginHint) {
         // authorization_endpoint
-        var builder = UriComponentsBuilder.fromUri(URI.create(damFacadeConfig.getOauth2AuthorizationUrl()))
-                .queryParam("response_type", "code")
-                .queryParam("client_id", damFacadeConfig.getClientId())
-                .queryParam("redirect_uri", redirectUri)
-                .queryParam("resource", damFacadeConfig.getWesResourceId())
-                .queryParam("nonce", state)
-                .queryParam("state", state)
-                .queryParam("ttl", ttl);
+        var builder = UriComponentsBuilder.fromUri(URI.create(oauth2Config.getAuthorizationUrl()))
+            .queryParam("response_type", "code")
+            .queryParam("client_id", oauth2Config.getClientId())
+            .queryParam("redirect_uri", redirectUri)
+            .queryParam("resource", damFacadeConfig.getWesResourceId())
+            .queryParam("nonce", state)
+            .queryParam("state", state);
 
-        if (scopes != null) {
+        if (scopes != null && !scopes.equals("")) {
             builder.queryParam("scope", scopes);
+        } else if (oauth2Config.getDefaultScope() != null) {
+            builder.queryParam("scope", oauth2Config.getDefaultScope());
         }
         if (loginHint != null) {
             builder.queryParam("login_hint", loginHint);
@@ -54,16 +58,17 @@ public class ReactiveDamOAuthFacadeClient implements ReactiveDamOAuthClient {
     public Mono<TokenResponse> exchangeAuthorizationCodeForTokens(String realm, URI redirectUri, String code) {
         // NOTE: Based on BaseReactiveOauthClient
         return WebClientFactory.getWebClient()
-                .post()
-                .uri(damFacadeConfig.getOauth2TokenUrl())
-                .header(AUTHORIZATION, "Basic " + encodeBasicAuth(damFacadeConfig.getClientId(), damFacadeConfig.getClientSecret()))
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters.fromFormData("grant_type", "authorization_code")
-                        .with("redirect_uri", redirectUri.toString())
-                        .with("code", code))
-                .exchange()
-                .flatMap(this::extractIdpTokens)
-                .onErrorMap(ex ->  new InvalidTokenException(ex.getMessage()));
+            .post()
+            .uri(oauth2Config.getTokenUrl())
+            .header(AUTHORIZATION,
+                "Basic " + encodeBasicAuth(oauth2Config.getClientId(), oauth2Config.getClientSecret()))
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(BodyInserters.fromFormData("grant_type", "authorization_code")
+                //                        .with("redirect_uri", redirectUri.toString())
+                .with("code", code))
+            .exchange()
+            .flatMap(this::extractIdpTokens)
+            .onErrorMap(ex -> new InvalidTokenException(ex.getMessage()));
     }
 
     @Override
@@ -89,7 +94,7 @@ public class ReactiveDamOAuthFacadeClient implements ReactiveDamOAuthClient {
     private String encodeBasicAuth(String user, String password) {
         // NOTE: Copied from BaseReactiveOauthClient
         return Base64.getEncoder()
-                .encodeToString((user + ":" + password).getBytes());
+            .encodeToString((user + ":" + password).getBytes());
     }
 
     private Mono<TokenResponse> extractIdpTokens(ClientResponse idpTokenResponse) {
@@ -98,15 +103,15 @@ public class ReactiveDamOAuthFacadeClient implements ReactiveDamOAuthClient {
             return idpTokenResponse.bodyToMono(TokenResponse.class);
         } else {
             return idpTokenResponse.bodyToMono(String.class)
-                    .flatMap(errorBody -> Mono.error(new TokenExchangeException(errorBody)));
+                .flatMap(errorBody -> Mono.error(new TokenExchangeException(errorBody)));
         }
     }
 
     private static boolean contentTypeIsApplicationJson(ClientResponse response) {
         // NOTE: Copied from BaseReactiveOauthClient
         return response.headers()
-                .contentType()
-                .filter(mediaType -> mediaType.isCompatibleWith(APPLICATION_JSON))
-                .isPresent();
+            .contentType()
+            .filter(mediaType -> mediaType.isCompatibleWith(APPLICATION_JSON))
+            .isPresent();
     }
 }

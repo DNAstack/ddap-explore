1519e5954d844f1b2772609c5ac4212d49d9cc1d
Monica <monica@dnastack.com>
2020-04-06 13:55:41 -0400

    DISCO-2779 update explore ui to display key value pairs

--- angular/src/app/shared/view-access/view-access.component.ts
+++ angular/src/app/shared/view-access/view-access.component.ts
@@ -1,18 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import IResourceAccess = dam.v1.ResourceResults.IResourceAccess;
+import _startcase from 'lodash.startcase';
 
 import { dam } from '../proto/dam-service';
-import IResourceAccess = dam.v1.ResourceResults.IResourceAccess;
 
 @Component({
   selector: 'ddap-view-access',
   templateUrl: './view-access.component.html',
   styleUrls: ['./view-access.component.scss'],
 })
-export class ViewAccessComponent {
+export class ViewAccessComponent implements OnInit {
 
   @Input()
   access: IResourceAccess;
   @Input()
   url?: string;
+  credentials: object[] = [];
+
+  ngOnInit(): void {
+    if (this.access.credentials) {
+      Object.entries(this.access.credentials)
+        .map(([name, credVal]) => {
+          if (credVal.length > 0) {
+            this.credentials.push({
+              label: this.formatVal(name),
+              value: credVal,
+            });
+          }
+        });
+    }
+  }
+
+  // format keys with _ to Start Case
+  private formatVal(val: string) {
+    return /^\w*$/.test(val) ? _startcase(val) : val;
+  }
 
 }

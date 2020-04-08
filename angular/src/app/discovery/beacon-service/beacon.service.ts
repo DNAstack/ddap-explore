6d22d4c8b48c84a6a279a114ee4d3f7aac008464
Juti Noppornpitak <jnopporn@shiroyuki.com>
2020-04-08 12:08:48 -0400

    Fixed the incorrect response modeling

--- angular/src/app/discovery/beacon-service/beacon.service.ts
+++ angular/src/app/discovery/beacon-service/beacon.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { BeaconResponse } from './beacon.model';
+import { BeaconAPIResponse, BeaconResponse } from './beacon.model';
 
 @Injectable({
   providedIn: 'root',
@@ -33,7 +33,7 @@ export class BeaconService {
     referenceBases: string,
     alternateBases: string,
     headers?: HttpHeaders)
-    : Promise<BeaconResponse[]> { // FIXME This is an incorrect response type. Need to use BeaconAPIResponse instead.
+    : Promise<BeaconAPIResponse> {
     return this.searchBeacons(
       assembly,
       referenceName,
@@ -50,7 +50,7 @@ export class BeaconService {
     referenceBases: string,
     alternateBases: string,
     headers?: HttpHeaders)
-    : Promise<BeaconResponse[]> {
+    : Promise<BeaconAPIResponse> {
 
     const params = new HttpParams()
       .set('assemblyId', assembly)
@@ -61,7 +61,7 @@ export class BeaconService {
     ;
 
     // /query?referenceName=1&start=9924&referenceBases=C&alternateBases=T&assemblyId=GRCh38
-    return this.httpClient.get<BeaconResponse[]>(`${this.apiUrl}`
+    return this.httpClient.get<BeaconAPIResponse>(`${this.apiUrl}`
       + '/query', {'params': params, 'headers': headers}).toPromise();
   }
 }

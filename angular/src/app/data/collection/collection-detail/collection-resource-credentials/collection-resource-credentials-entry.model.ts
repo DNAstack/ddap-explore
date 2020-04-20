bb58685b996382b70b6e6e1846c0e170547a63ef
Milan Panik <milan.panik1@gmail.com>
2020-04-20 15:32:18 +0200

    DISCO-2795 Refactor resource access components

---
+++ angular/src/app/data/collection/collection-detail/collection-resource-credentials/collection-resource-credentials-entry.model.ts
@@ -0,0 +1,5 @@
+export interface CollectionResourceCredentialsEntryModel {
+  label: string;
+  value: string;
+  key: string;
+}
--- angular/src/app/data/collection/collections.module.ts
+++ angular/src/app/data/collection/collections.module.ts
@@ -7,6 +7,11 @@ import { BeaconResultComponent } from './collection-beacon-search/beacon-result/
 import { BeaconSearchBarComponent } from './collection-beacon-search/beacon-search-bar/beacon-search-bar.component';
 import { CollectionBeaconSearchComponent } from './collection-beacon-search/collection-beacon-search.component';
 import { CollectionDetailComponent } from './collection-detail/collection-detail.component';
+import { CollectionLogoComponent } from './collection-detail/collection-logo/collection-logo.component';
+import {
+  CollectionResourceCredentialsComponent
+} from './collection-detail/collection-resource-credentials/collection-resource-credentials.component';
+import { CollectionResourceComponent } from './collection-detail/collection-resource/collection-resource.component';
 import { CollectionListComponent } from './collection-list/collection-list.component';
 import { CollectionsRoutingModule } from './collections-routing.module';
 
@@ -15,6 +20,9 @@ import { CollectionsRoutingModule } from './collections-routing.module';
     CollectionListComponent,
     CollectionDetailComponent,
     CollectionBeaconSearchComponent,
+    CollectionLogoComponent,
+    CollectionResourceComponent,
+    CollectionResourceCredentialsComponent,
     BeaconSearchBarComponent,
     BeaconResultComponent,
     BeaconLimitSearchComponent,

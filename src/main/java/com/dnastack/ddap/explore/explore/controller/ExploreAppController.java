f7b5ba588a08361ef2a13e7edf1425093cb41694
patrick <patrick@dnastack.com>
2020-04-20 16:30:56 -0400

    Cleaned up Controllers and moved encryption to userTokenService

--- src/main/java/com/dnastack/ddap/explore/explore/controller/ExploreAppController.java
+++ src/main/java/com/dnastack/ddap/explore/explore/controller/ExploreAppController.java
@@ -4,7 +4,6 @@ import com.dnastack.ddap.common.util.http.XForwardUtil;
 import com.dnastack.ddap.explore.explore.model.ExploreTokenResponse;
 import com.dnastack.ddap.explore.resource.model.UserCredential;
 import com.dnastack.ddap.explore.resource.service.UserCredentialService;
-import com.dnastack.ddap.explore.common.session.SessionEncryptionUtils;
 import java.net.URI;
 import java.time.Duration;
 import java.time.ZonedDateTime;
@@ -14,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpCookie;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,20 +29,23 @@ import reactor.core.publisher.Mono;
 public class ExploreAppController {
 
 
+    private final UserCredentialService userCredentialService;
+
     @Autowired
-    private UserCredentialService userCredentialService;
+    public ExploreAppController(UserCredentialService userCredentialService) {
+        this.userCredentialService = userCredentialService;
+    }
 
     @GetMapping(value = "/tokens", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<ExploreTokenResponse> getTokensForResources(ServerHttpRequest httpRequest, WebSession session, @PathVariable("realm") String realm,
         @RequestParam("resource") List<String> authorizationIds,
-        @RequestParam(value = "minimum_ttl",defaultValue = "10m") String minimumTtl) {
+        @RequestParam(value = "minimum_ttl", defaultValue = "10m") String minimumTtl) {
 
         return Mono.defer(() -> {
             final Duration enforceTtl = parseDuration(minimumTtl);
-            final String privateKeyString = requirePrivateKeyInCookie(httpRequest);
             final ExploreTokenResponse tokenResponse = new ExploreTokenResponse();
             List<UserCredential> existingCredentials = userCredentialService
-                .getSessionBoundTokens(session, authorizationIds);
+                .getAndDecryptSessionBoundTokens(httpRequest, session, authorizationIds);
             List<String> reauthenticate = new ArrayList<>();
             authorizationIds.forEach(id -> {
                 Optional<UserCredential> existingOpt = existingCredentials.stream()
@@ -68,21 +69,14 @@ public class ExploreAppController {
                 tokenResponse.setAuthorizationUrlBase(componentsBuilder.build().toUri());
             } else {
                 Map<String, String> tokens = new HashMap<>();
-                existingCredentials.forEach(credential -> {
-                    String encryptedToken = credential.getToken();
-                    String id = credential.getAuthorizationId();
-                    tokens.put(id, SessionEncryptionUtils.decryptData(privateKeyString, encryptedToken));
-
-                });
+                existingCredentials
+                    .forEach(credential -> tokens.put(credential.getAuthorizationId(), credential.getToken()));
                 tokenResponse.setTokens(tokens);
             }
 
             return Mono.just(tokenResponse);
         });
-
-
     }
-
 
     private Duration parseDuration(String period) {
         try {
@@ -94,16 +88,6 @@ public class ExploreAppController {
         } catch (Exception e) {
             return Duration.ofHours(1);
         }
-    }
-
-    private String requirePrivateKeyInCookie(ServerHttpRequest request) {
-        HttpCookie privateKey = request.getCookies().getFirst(SessionEncryptionUtils.COOKIE_NAME);
-        if (privateKey == null || privateKey.getValue() == null) {
-            throw new IllegalArgumentException(
-                "Could not extract the Session Decryption from the Cookie: " + SessionEncryptionUtils.COOKIE_NAME
-                    + ". Cookie does not exist");
-        }
-        return privateKey.getValue();
     }
 
 }
--- src/main/java/com/dnastack/ddap/explore/resource/service/UserCredentialService.java
+++ src/main/java/com/dnastack/ddap/explore/resource/service/UserCredentialService.java
@@ -1,9 +1,10 @@
 package com.dnastack.ddap.explore.resource.service;
 
-import com.dnastack.ddap.explore.resource.controller.ResourceController.Id;
+import com.dnastack.ddap.explore.common.session.PersistantSession;
+import com.dnastack.ddap.explore.common.session.SessionEncryptionUtils;
+import com.dnastack.ddap.explore.resource.model.Id;
 import com.dnastack.ddap.explore.resource.data.UserCredentialDao;
 import com.dnastack.ddap.explore.resource.model.UserCredential;
-import com.dnastack.ddap.explore.common.session.PersistantSession;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,8 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.WebSession;
@@ -31,36 +34,50 @@ public class UserCredentialService {
         return userIdentifier;
     }
 
-
     public Optional<UserCredential> getSessionBoundTokenForResource(WebSession session, Id resourceId) {
         return jdbi.withExtension(UserCredentialDao.class, dao -> dao
             .getCredentialForResource(getUserIdentifier(session), resourceId.encodeId()));
     }
 
-    public List<UserCredential> getSessionBoundTokens(WebSession session) {
+    public List<UserCredential> getSessionBoundTokens(WebSession session, List<String> authorizationIds) {
         return jdbi.withExtension(UserCredentialDao.class, dao -> dao
-            .getUserCredentials(getUserIdentifier(session)));
+            .getCredentialsForResources(getUserIdentifier(session), authorizationIds));
     }
 
-    public List<UserCredential> getSessionBountdokens(WebSession session, List<Id> resourceIds) {
-        List<String> ids = resourceIds.stream().map(Id::encodeId).collect(Collectors.toList());
-        return getSessionBoundTokens(session, ids);
-    }
-
-    public List<UserCredential> getSessionBoundTokens(WebSession session, List<String> ids) {
+    public List<UserCredential> getAndDecryptSessionBoundTokens(ServerHttpRequest request, WebSession session, List<String> authorizationIds) {
         return jdbi.withExtension(UserCredentialDao.class, dao -> dao
-            .getCredentialsForResources(getUserIdentifier(session), ids));
+            .getCredentialsForResources(getUserIdentifier(session), authorizationIds))
+            .stream().map(userCredential -> decryptSessionBoundToken(request, userCredential))
+            .collect(Collectors.toList());
     }
 
+
+    private UserCredential decryptSessionBoundToken(ServerHttpRequest request, UserCredential userCredential) {
+        String privateKey = requirePrivateKeyInCookie(request);
+        String decryptedToken = SessionEncryptionUtils.decryptData(privateKey, userCredential.getToken());
+        userCredential.setToken(decryptedToken);
+        return userCredential;
+    }
+
+    private String requirePrivateKeyInCookie(ServerHttpRequest request) {
+        HttpCookie privateKey = request.getCookies().getFirst(SessionEncryptionUtils.COOKIE_NAME);
+        if (privateKey == null || privateKey.getValue() == null) {
+            throw new IllegalArgumentException(
+                "Could not extract the Session Decryption from the Cookie: " + SessionEncryptionUtils.COOKIE_NAME
+                    + ". Cookie does not exist");
+        }
+        return privateKey.getValue();
+    }
 
     public void storeSessionBoundTokenForResource(WebSession session, Id authorizationId, ZonedDateTime expires, String token) {
+        String encryptedToken = SessionEncryptionUtils.encryptData(session, token);
         String principal = getUserIdentifier(session);
         UserCredential credential = new UserCredential();
         credential.setCreationTime(ZonedDateTime.now());
         credential.setPrincipalId(principal);
         credential.setAuthorizationId(authorizationId.encodeId());
         credential.setExpirationTime(expires != null ? expires : ZonedDateTime.now().plusHours(1));
-        credential.setToken(token);
+        credential.setToken(encryptedToken);
 
         jdbi.useExtension(UserCredentialDao.class, dao -> {
             dao.getCredentialForResource(principal, credential.getAuthorizationId()).ifPresent(_cred -> {

7397fd99d69857bd629821e9ef0be8be118e08aa
Milan Panik <milan.panik1@gmail.com>
2020-04-22 13:19:35 +0200

    DISCO-2795 Fix styles and update data explore e2e tests

--- e2e-tests/src/main/java/com/dnastack/ddap/common/fragments/ExpandedAccessibleViewItem.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/common/fragments/ExpandedAccessibleViewItem.java
@@ -3,12 +3,15 @@ package com.dnastack.ddap.common.fragments;
 import com.dnastack.ddap.common.util.DdapBy;
 import com.dnastack.ddap.common.util.WebPageScroller;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URI;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.isEmptyString;
@@ -38,6 +41,32 @@ public class ExpandedAccessibleViewItem {
         WebPageScroller.scrollTo(driver, accessTokenInput);
         String accessToken = accessTokenInput.getAttribute("value");
         assertThat(accessToken, not(isEmptyString()));
+    }
+
+    public void fillFieldFromDropdown(By fieldSelector, String fieldValue) {
+        WebElement field = driver.findElement(fieldSelector);
+
+        new WebDriverWait(driver, 5)
+            .until(ExpectedConditions.elementToBeClickable(field));
+        // This dismisses any previous auto-complete suggestions in other fields.
+        field.sendKeys(Keys.ENTER);
+
+        List<WebElement> options = driver.findElements(By.tagName("mat-option"));
+
+        if (fieldValue != null) {
+            WebElement option =
+                new WebDriverWait(driver, 5)
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                        "//mat-option/span[contains(text(), '" + fieldValue + "')]")));
+
+            option.click();
+        } else {
+            options.get(0).click();
+        }
+    }
+
+    public void fillFieldWithFirstValueFromDropdown(By fieldSelector) {
+        fillFieldFromDropdown(fieldSelector, null);
     }
 
     public String getDownloadLink() {

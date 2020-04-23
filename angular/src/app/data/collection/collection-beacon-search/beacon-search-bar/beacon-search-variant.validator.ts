466ead5bf56bdf5fd87ec031c64bfd361b0c58de
Milan Panik <milan.panik1@gmail.com>
2020-04-23 11:41:34 +0200

    DISCO-2795 Fix variant validator and support beacon search on ENTER key

--- angular/src/app/data/collection/collection-beacon-search/beacon-search-bar/beacon-search-variant.validator.ts
+++ angular/src/app/data/collection/collection-beacon-search/beacon-search-bar/beacon-search-variant.validator.ts
@@ -5,13 +5,17 @@ import { BeaconSearchQueryParser } from '../beacon-search-query.parser';
 export class BeaconSearchVariantValidator {
 
   static variant(control: AbstractControl) {
-    if (!BeaconSearchQueryParser.validate(control.value)) {
-      return { validLocus: true };
+    if (!control || !control.value || control.value === '') {
+      return null;
+    }
+
+    if (BeaconSearchQueryParser.validate(control.value)) {
+      return null;
     }
 
     return {
       // locus - location on a chromosome
-      validLocus: true,
+      invalidLocus: true,
     };
   }
 

8fb1eb47a57243cbb072861bb157943e6c402ebb
patrick <patrick@dnastack.com>
2020-04-27 15:12:04 -0400

    Added additional documentation on resource controller

--- src/main/java/com/dnastack/ddap/explore/resource/controller/ResourceController.java
+++ src/main/java/com/dnastack/ddap/explore/resource/controller/ResourceController.java
@@ -57,11 +57,22 @@ public class ResourceController {
     }
 
 
+    /**
+     * List all resources across all configured resource clients. Passing in all filters.
+     *
+     * @param realm The realm this request was submitted under
+     * @param collectionsToFilter An array of String Ids of collections to filter resources by
+     * @param interfaceTypesToFilter An array of interface types to filter results by. Interface types will match based
+     * on both an exact match as well as a substring
+     * @param interfaceUrisToFilter An array of interface uris to filter reults by.
+     * @return Paginated Response
+     */
     @GetMapping(value = "/{realm}/resources", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<PaginatedResponse<Resource>> listResources(@PathVariable("realm") String realm, @RequestParam(value = "collection", required = false) List<String> collections, @RequestParam(value = "interface_type", required = false) List<String> interfaceTypesToFilter, @RequestParam(value = "interface_uri", required = false) List<String> interfaceUrisToFilter, @RequestParam(value = "page_token", required = false) String pageToken) {
+    public Mono<PaginatedResponse<Resource>> listResources(@PathVariable("realm") String realm, @RequestParam(value = "collection", required = false) List<String> collectionsToFilter, @RequestParam(value = "interface_type", required = false) List<String> interfaceTypesToFilter, @RequestParam(value = "interface_uri", required = false) List<String> interfaceUrisToFilter, @RequestParam(value = "page_token", required = false) String pageToken) {
         List<CollectionId> collectionIdsToFilter = new ArrayList<>();
-        if (collections != null) {
-            collectionIdsToFilter.addAll(collections.stream().map(Id::decodeCollectionId).collect(Collectors.toList()));
+        if (collectionsToFilter != null) {
+            collectionIdsToFilter
+                .addAll(collectionsToFilter.stream().map(Id::decodeCollectionId).collect(Collectors.toList()));
         }
         return resourceClientService
             .listResources(realm, collectionIdsToFilter, interfaceTypesToFilter, interfaceUrisToFilter)
@@ -69,6 +80,12 @@ public class ResourceController {
 
     }
 
+    /**
+     * Get a specific resource by its ID
+     *
+     * @param realm The realm this request was submitted under
+     * @param resourceId the ID of the resource
+     */
     @GetMapping(value = "/{realm}/resources/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Resource> getResource(@PathVariable("realm") String realm, @PathVariable("id") String resourceId) {
         ResourceId id = Id.decodeResourceId(resourceId);
@@ -76,17 +93,52 @@ public class ResourceController {
     }
 
 
+    /**
+     * List all Collectgions across all configured resource clients
+     *
+     * @param realm The realm this request was submitted under
+     */
     @GetMapping(value = "/{realm}/collections", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<PaginatedResponse<Collection>> listCollections(@PathVariable("realm") String realm) {
         return resourceClientService.listCollections(realm).map(PaginatedResponse::new);
     }
 
+    /**
+     * Get a specific Collection by its ID
+     *
+     * @param realm The realm this request was submitted under
+     * @param collectionId the Id of the collection
+     */
     @GetMapping(value = "/{realm}/collections/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Collection> getCollection(@PathVariable("realm") String realm, @PathVariable("id") String collectionId) {
         CollectionId id = Id.decodeCollectionId(collectionId);
         return resourceClientService.getClient(id.getSpiKey()).getCollection(realm, id);
     }
 
+    /**
+     * Start an authorization flow for one or more resources, persisting state in between in the user's session. If more
+     * then one resource is provided which requires authorization from a different {@link ResourceClient} then the user
+     * will need to complete at least <code>N</code> authorization fliows, where <code>N</code> is the number of
+     * configured clients. Additionally, each client may require multiple Authorization flows to be conducted as well.
+     *
+     * Authorization flows will be stored in the user's session, and must be done in the order that they are speciefied.
+     * Upon successfull completion of a a single flow, the user will be redirected to the next flow, based on whetehr or
+     * not there is a {@link OAuthState#getNextState()} attribute set. Authorization Flows cannot be replayed and will
+     * be cleared from the user's session once the callback url has been called.
+     *
+     * If there are no resources which require authorization, the user will be immediately redirected to their final
+     * destination page
+     *
+     * @param request The Http Request
+     * @param session The user's session
+     * @param realm The Realm this request was submitted under
+     * @param loginHint A login hint which will be passed to the underlying Resource Client
+     * @param redirectUri The final redirect URI
+     * @param scope Additional scopes to pass to the Resource Clien
+     * @param interfaceIds A list of interfaceId's to authorize
+     * @param ttl The TTL to request from the resource clients
+     * @return Redirect
+     */
     @GetMapping("/{realm}/resources/authorize")
     public ResponseEntity<?> authorizeResources(WebSession session, ServerHttpRequest request,
         @PathVariable String realm,
@@ -126,6 +178,27 @@ public class ResourceController {
 
     }
 
+
+    /**
+     * Callback phase of an authorization codeflow
+     * <p/>
+     * Upon successful completion of the Authorization Code flow, The user's state will be verified and reset, ensuring
+     * that the request cannot be subject to a replay attack. Once the state has been verified, the final stages of the
+     * Authorization flow are delegated to the specific {@link ResourceClient} Matching the current resource batch that
+     * the request is being performed for, yielding a list of {@link com.dnastack.ddap.explore.resource.model.UserCredential}'s
+     * which will be persisted for the current user's session.
+     *
+     * If there is no <code>nextState</code> stored in the current state, then the user will be redirected to the final
+     * login page. However if there IS a <code>nextState</code> the user will be redirected to perform the next
+     * authorization flow
+     *
+     * @param request The HTTP Request
+     * @param session The User's Session
+     * @param code The Authorization Code to exchange for a token
+     * @param error The Error (if any) which was encountered
+     * @param errorDescription the description of the error
+     * @return Redirect to the final
+     */
     @GetMapping("/_/resources/authorize/callback")
     public Mono<? extends ResponseEntity<?>> handleTokenRequest(ServerHttpRequest request, WebSession session,
         @RequestParam(required = false) String code,
@@ -133,7 +206,6 @@ public class ResourceController {
         @RequestParam(value = "error_description", required = false) String errorDescription) {
 
         return Mono.defer(() -> {
-
             final OAuthState storedState = verifyStateAndClearSession(request, session);
             final ResourceClient resourceClient = resourceClientService.getClient(storedState.getSpiKey());
             final URI redirectUri = getRedirectUri(request);

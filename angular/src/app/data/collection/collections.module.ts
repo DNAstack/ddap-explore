5c2f5aaccf4a64faf056d15b166a8e3baec1643f
Milan Panik <milan.panik1@gmail.com>
2020-05-04 21:28:23 +0200

    DISCO-2795 Remove obsolete beacon serach for collections

--- angular/src/app/data/collection/collections.module.ts
+++ angular/src/app/data/collection/collections.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { DataSharedModule } from '../shared/shared.module';
 
-import { BeaconLimitSearchComponent } from './collection-beacon-search/beacon-limit-search/beacon-limit-search.component';
-import { BeaconResultComponent } from './collection-beacon-search/beacon-result/beacon-result.component';
-import { BeaconSearchBarComponent } from './collection-beacon-search/beacon-search-bar/beacon-search-bar.component';
-import { CollectionBeaconSearchComponent } from './collection-beacon-search/collection-beacon-search.component';
 import { CollectionDetailComponent } from './collection-detail/collection-detail.component';
 import { CollectionLogoComponent } from './collection-detail/collection-logo/collection-logo.component';
 import {
@@ -19,13 +15,9 @@ import { CollectionsRoutingModule } from './collections-routing.module';
   declarations: [
     CollectionListComponent,
     CollectionDetailComponent,
-    CollectionBeaconSearchComponent,
     CollectionLogoComponent,
     ResourceAccessFormComponent,
     ResourceAccessCredentialsComponent,
-    BeaconSearchBarComponent,
-    BeaconResultComponent,
-    BeaconLimitSearchComponent,
   ],
   imports: [
     DataSharedModule,

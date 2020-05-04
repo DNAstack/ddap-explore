3b7b80a6aa59a27ce03b5a6caa96c50f7824dbb2
Milan Panik <milan.panik1@gmail.com>
2020-05-04 21:28:23 +0200

    DISCO-2795 Remove obsolete beacon serach for collections

--- angular/src/app/data/collection/collections-routing.module.ts
+++ angular/src/app/data/collection/collections-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CollectionBeaconSearchComponent } from './collection-beacon-search/collection-beacon-search.component';
 import { CollectionDetailComponent } from './collection-detail/collection-detail.component';
 import { CollectionListComponent } from './collection-list/collection-list.component';
 
 export const routes: Routes = [
   { path: '', component: CollectionListComponent },
-  { path: 'search', component: CollectionBeaconSearchComponent, pathMatch: 'full' },
   { path: ':collectionId', component: CollectionDetailComponent },
 ];
 

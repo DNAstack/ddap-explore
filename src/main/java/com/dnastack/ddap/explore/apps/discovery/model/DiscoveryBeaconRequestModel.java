68b0519a88186b803cc8cc1f09050bf16c40ae3c
Milan Panik <milan.panik1@gmail.com>
2020-05-05 17:28:49 +0200

    DISCO-2795 Support sending datasetIds to beacon

--- src/main/java/com/dnastack/ddap/explore/apps/discovery/model/DiscoveryBeaconRequestModel.java
+++ src/main/java/com/dnastack/ddap/explore/apps/discovery/model/DiscoveryBeaconRequestModel.java
@@ -2,13 +2,16 @@ package com.dnastack.ddap.explore.apps.discovery.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class DiscoveryBeaconRequestModel {
 
-    String assemblyId;
-    String referenceName;
-    String start;
-    String referenceBases;
-    String alternateBases;
+    private String assemblyId;
+    private String referenceName;
+    private String start;
+    private String referenceBases;
+    private String alternateBases;
+    private List<String> datasetIds;
 
 }

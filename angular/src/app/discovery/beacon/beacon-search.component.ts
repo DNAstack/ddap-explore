67e8a5e286ee31428a1b3f89b777e50ec08662e1
Milan Panik <milan.panik1@gmail.com>
2020-05-07 10:43:59 +0200

    DISCO-2841 Hide dataset and beacon selects only when in standalone mode

--- angular/src/app/discovery/beacon/beacon-search.component.ts
+++ angular/src/app/discovery/beacon/beacon-search.component.ts
@@ -3,8 +3,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { ErrorHandlerService } from 'ddap-common-lib';
 import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
-import { catchError, filter, switchMap } from 'rxjs/operators';
+import { catchError, filter, map, switchMap } from 'rxjs/operators';
 
+import { AppConfigStore } from '../../shared/app-config/app-config.store';
 import { AppDiscoveryService } from '../../shared/apps/app-discovery/app-discovery.service';
 import { BeaconQueryAlleleRequestModel, BeaconQueryResponseModel } from '../../shared/beacon/beacon-search.model';
 
@@ -21,11 +22,13 @@ export class BeaconSearchComponent implements OnInit {
   beaconForm: BeaconInfoFormModel;
   beaconQuery: BeaconQueryAlleleRequestModel;
   beaconQueryResponse$: Observable<BeaconQueryResponseModel>;
+  isStandaloneMode$: Observable<boolean>;
 
   private readonly refreshBeaconResult$ = new BehaviorSubject<BeaconQueryAlleleRequestModel>(undefined);
 
   constructor(
     public loader: LoadingBarService,
+    private appConfigStore: AppConfigStore,
     private appDiscoveryService: AppDiscoveryService,
     private errorHandlerService: ErrorHandlerService,
     private helpDialog: MatDialog
@@ -33,21 +36,14 @@ export class BeaconSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.beaconQueryResponse$ = this.refreshBeaconResult$.pipe(
-      filter((params: BeaconQueryAlleleRequestModel) => {
-        return params && params.datasetIds && params.datasetIds.length > 0;
-      }),
-      switchMap((params: BeaconQueryAlleleRequestModel) => {
-        const interfaceId = this.beaconForm.beacon.resource.interfaces[0].id;
-        return this.appDiscoveryService.queryBeacon(interfaceId, params)
-          .pipe(
-            catchError((error) => {
-              this.errorHandlerService.openSnackBarWithError(error, 'error.message');
-              return EMPTY;
-            })
-          );
-      })
-    );
+    this.isStandaloneMode$ = this.appConfigStore.state$
+      .pipe(
+        map((appConfig) => {
+          return appConfig.inStandaloneMode;
+        })
+      );
+
+    this.setUpBeaconQueryObservable();
   }
 
   openHelpDialog(): void {
@@ -66,6 +62,24 @@ export class BeaconSearchComponent implements OnInit {
 
   private submitQuery(): void {
     this.refreshBeaconResult$.next({ ...this.beaconQuery, datasetIds: this.beaconForm.datasets });
+  }
+
+  private setUpBeaconQueryObservable() {
+    this.beaconQueryResponse$ = this.refreshBeaconResult$.pipe(
+      filter((params: BeaconQueryAlleleRequestModel) => {
+        return params && params.datasetIds && params.datasetIds.length > 0;
+      }),
+      switchMap((params: BeaconQueryAlleleRequestModel) => {
+        const interfaceId = this.beaconForm.beacon.resource.interfaces[0].id;
+        return this.appDiscoveryService.queryBeacon(interfaceId, params)
+          .pipe(
+            catchError((error) => {
+              this.errorHandlerService.openSnackBarWithError(error, 'error.message');
+              return EMPTY;
+            })
+          );
+      })
+    );
   }
 
 }

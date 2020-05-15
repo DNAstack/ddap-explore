e595db81ae2e40147e4ff63446adf05d4594ac88
Milan Panik <milan.panik1@gmail.com>
2020-05-15 11:50:58 +0200

    DISCO-2851 Support authRequired beacons

--- angular/src/app/discovery/beacon/beacon-search.component.ts
+++ angular/src/app/discovery/beacon/beacon-search.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDrawer } from '@angular/material/sidenav';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { ErrorHandlerService } from 'ddap-common-lib';
 import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
 import { catchError, filter, map, switchMap } from 'rxjs/operators';
 
+import { environment } from '../../../environments/environment';
 import { AppConfigStore } from '../../shared/app-config/app-config.store';
 import { AppDiscoveryService } from '../../shared/apps/app-discovery/app-discovery.service';
 import { BeaconQueryAlleleRequestModel, BeaconQueryResponseModel } from '../../shared/beacon/beacon-search.model';
@@ -35,6 +37,8 @@ export class BeaconSearchComponent implements OnInit {
 
   constructor(
     public loader: LoadingBarService,
+    private route: ActivatedRoute,
+    private router: Router,
     private appConfigStore: AppConfigStore,
     private appDiscoveryService: AppDiscoveryService,
     private errorHandlerService: ErrorHandlerService,
@@ -75,6 +79,13 @@ export class BeaconSearchComponent implements OnInit {
   closeDetail(): void {
     this.selectedRowDetailDrawer.close();
     this.dataTableEventsService.deselectRows();
+  }
+
+  buildUrlForResourceAuthorization(authorizationUrlBase: string): string {
+    // TODO: add redirect with preselected beacon which was requested for authorization
+    //       OR save form state before clicking Authorize and always load saved state
+    return `${authorizationUrlBase}`
+      + `&redirect_uri=${encodeURIComponent(this.router.url)}`;
   }
 
   private setUpBeaconQueryObservable() {
